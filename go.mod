08f916170df63ec4e1a8e78ab3c5a6b793bec4e9
Luis Fernando C. Ferreira <raintech@outlook.com>
2025-07-21 22:38:22 -0300

    fix(tests): load .env file for test execution

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.3
 	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.10.0
 	golang.org/x/crypto v0.40.0
--- internal/handlers/main_test.go
+++ internal/handlers/main_test.go
@@ -1,31 +1,31 @@
 package handlers
 
 import (
+	"log"
 	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/raingrave/apirest/internal"
 )
 
 var router *gin.Engine
 
 func TestMain(m *testing.M) {
-	// Set Gin to test mode
 	gin.SetMode(gin.TestMode)
 
-	// Setup database connection
-	// Note: This will use the same DB as dev, but we will truncate tables.
-	// For a real-world scenario, consider a separate test database.
+	err := godotenv.Load("../../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file for tests")
+	}
+
 	internal.ConnectDB()
 
-	// Setup router
 	router = setupRouter()
 
-	// Run tests
 	code := m.Run()
 
-	// Exit
 	os.Exit(code)
 }
 
